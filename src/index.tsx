882d16456761cec85f46d4dcc9fb6a454134d9cb
Amandeep Rehal <arehal4@gmail.com>
2021-03-09 15:23:00 -0800

    added color theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      "100": "#a2cfa3",
+      "400": "#87c188",
+      "500": "#3f9b40",
+      "600": "#3f9b40",
+      "700": "#0a7e0c",
+      "800": "#064a07",
+      "900": "#064a07",
+    },
+  },
+});
 
 ReactDOM.render(
   <ChakraProvider>
